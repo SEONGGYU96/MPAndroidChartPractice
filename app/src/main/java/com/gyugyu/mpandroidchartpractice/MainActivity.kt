f1a9e1c2c81e57c23753f2bc281ef5e791deaf13
김성규 <link5658@gmail.com>
2020-11-04 02:16:24 +0900

    feat: Draw a basic random line graph

--- app/src/main/java/com/gyugyu/mpandroidchartpractice/MainActivity.kt
+++ app/src/main/java/com/gyugyu/mpandroidchartpractice/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.gyugyu.mpandroidchartpractice
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
+import com.gyugyu.mpandroidchartpractice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        //define
+        val chart = binding.linechartMain
+        val values = mutableListOf<Entry>()
+
+        //init random values for vertices
+        for (i in 0 until 10) {
+            val value = Math.random().toFloat() * 10
+            values.add(Entry(i.toFloat(), value))
+        }
+
+        //init data set
+        val set = LineDataSet(values, "DataSet 1")
+        val dataSets = mutableListOf<ILineDataSet>().apply { this.add(set) }
+        val data = LineData(dataSets)
+
+        //customize appearance of chart
+        set.run {
+            color = Color.BLACK
+            setCircleColor(Color.BLACK)
+        }
+
+        //set data to chart
+        chart.data = data
     }
 }
