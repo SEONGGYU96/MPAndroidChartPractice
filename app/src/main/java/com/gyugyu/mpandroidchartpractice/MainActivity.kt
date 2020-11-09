8dd79f2a7d1c887781996e1c78f054e0b6d0ee91
김성규 <link5658@gmail.com>
2020-11-09 19:01:44 +0900

    feat: Design like a ECG monitor

--- app/src/main/java/com/gyugyu/mpandroidchartpractice/MainActivity.kt
+++ app/src/main/java/com/gyugyu/mpandroidchartpractice/MainActivity.kt
@@ -5,6 +5,7 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
@@ -20,29 +21,56 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         //define
-        val chart = binding.linechartMain
+        val chart = binding.linechartMain.apply {
+            isDragEnabled = false
+            description = null
+            setBackgroundColor(getColor(R.color.colorBackground))
+            setScaleEnabled(false)
+            setTouchEnabled(false)
+
+            xAxis.run {
+                isEnabled = true
+                position = XAxis.XAxisPosition.BOTTOM_INSIDE
+                axisLineColor = Color.BLACK
+                setDrawAxisLine(true)
+                setDrawGridLines(false)
+            }
+
+            axisLeft.run {
+                isEnabled = true
+                textColor = Color.BLACK
+                axisLineColor = Color.BLACK
+                labelCount = 0
+                setDrawGridLines(false)
+                setDrawAxisLine(true)
+            }
+
+            axisRight.isEnabled = false
+        }
+
         val values = mutableListOf<Entry>()
 
         //init random values for vertices
         for (i in 0 until 10) {
-            val value = Math.random().toFloat() * 10
+            val value = Math.random().toFloat() * 10 - 5
             values.add(Entry(i.toFloat(), value))
         }
 
         //init data set
-        val set = LineDataSet(values, "DataSet 1")
-        val dataSets = mutableListOf<ILineDataSet>().apply { this.add(set) }
-        val data = LineData(dataSets)
-
-        //customize appearance of chart
-        set.run {
+        val set = LineDataSet(values, "Dummy").apply {
+            //customize appearance of chart
             color = Color.BLACK
-            setCircleColor(Color.BLACK)
+            setDrawCircles(false)
+            setDrawValues(false)
+            label = null
+            color
         }
+        val dataSets = mutableListOf<ILineDataSet>().apply { add(set) }
 
         //set data to chart
-        chart.data = data
+        chart.data = LineData(dataSets)
 
+        //change orientation of screen
         binding.imagebuttonOrientationMain.setOnClickListener {
             changeScreenMode()
         }
