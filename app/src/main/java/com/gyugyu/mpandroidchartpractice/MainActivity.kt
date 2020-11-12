be94a546d78848c17321916f9ef4ab8fc90858d9
김성규 <link5658@gmail.com>
2020-11-12 20:48:30 +0900

    feat: Add data in real time using button

--- app/src/main/java/com/gyugyu/mpandroidchartpractice/MainActivity.kt
+++ app/src/main/java/com/gyugyu/mpandroidchartpractice/MainActivity.kt
@@ -2,26 +2,33 @@ package com.gyugyu.mpandroidchartpractice
 
 import android.content.pm.ActivityInfo
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.github.mikephil.charting.charts.LineChart
 import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
-import com.github.mikephil.charting.interfaces.datasets.ILineDataSet
 import com.gyugyu.mpandroidchartpractice.databinding.ActivityMainBinding
 
+
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val MAX = 50F
+    }
+
     private lateinit var binding: ActivityMainBinding
     private var currentOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+    private lateinit var chart: LineChart
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         //define
-        val chart = binding.linechartMain.apply {
+        chart = binding.linechartMain.apply {
             isDragEnabled = false
             description = null
             setBackgroundColor(getColor(R.color.colorBackground))
@@ -34,6 +41,9 @@ class MainActivity : AppCompatActivity() {
                 axisLineColor = Color.BLACK
                 setDrawAxisLine(true)
                 setDrawGridLines(false)
+                //set right padding of viewport
+                //for put a last vertex on center of viewport
+                spaceMax = MAX / 2
             }
 
             axisLeft.run {
@@ -46,33 +56,19 @@ class MainActivity : AppCompatActivity() {
             }
 
             axisRight.isEnabled = false
+
+            //add a empty data
+            data = LineData()
         }
-
-        val values = mutableListOf<Entry>()
-
-        //init random values for vertices
-        for (i in 0 until 10) {
-            val value = Math.random().toFloat() * 10 - 5
-            values.add(Entry(i.toFloat(), value))
-        }
-
-        //init data set
-        val set = LineDataSet(values, "Dummy").apply {
-            //customize appearance of chart
-            color = Color.BLACK
-            setDrawCircles(false)
-            setDrawValues(false)
-            label = null
-            color
-        }
-        val dataSets = mutableListOf<ILineDataSet>().apply { add(set) }
-
-        //set data to chart
-        chart.data = LineData(dataSets)
 
         //change orientation of screen
-        binding.imagebuttonOrientationMain.setOnClickListener {
-            changeScreenMode()
+        binding.run {
+            imagebuttonOrientationMain.setOnClickListener {
+                changeScreenMode()
+            }
+            imagebuttonAddVertexMain.setOnClickListener {
+                addEntry(Math.random() * 10 - 5)
+            }
         }
     }
 
@@ -84,5 +80,50 @@ class MainActivity : AppCompatActivity() {
                 ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
             }
         currentOrientation = requestedOrientation
+    }
+
+    private fun addEntry(num: Double) {
+        var data: LineData? = chart.data
+        if (data == null) {
+            data = LineData()
+            chart.data = data
+        }
+
+        var set = data.getDataSetByIndex(0)
+        // set.addEntry(...); // can be called as well
+        if (set == null) {
+            set = createSet()
+            data.addDataSet(set)
+        }
+
+        data.run {
+            //add random entry
+            addEntry(Entry(set.entryCount.toFloat(), num.toFloat()), 0)
+            notifyDataChanged()
+        }
+
+        refreshChart()
+    }
+
+    private fun refreshChart() {
+        // let the chart know it's data has changed
+        chart.run {
+            notifyDataSetChanged()
+            setVisibleXRangeMaximum(MAX)
+            setVisibleXRangeMinimum(MAX)
+            // this automatically refreshes the chart (calls invalidate())
+            moveViewToX(data.entryCount.toFloat())
+        }
+    }
+
+    private fun createSet(): LineDataSet {
+        //init data set
+        return LineDataSet(null, "Real-time Line Data").apply {
+            //customize appearance of line
+            color = Color.BLACK
+            label = null
+            setDrawCircles(false)
+            setDrawValues(false)
+        }
     }
 }
